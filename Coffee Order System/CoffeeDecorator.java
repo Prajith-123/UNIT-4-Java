710c30bca33aad0deb3e1fc3d404730a31cfe862
Prajith-123 <87675566+Prajith-123@users.noreply.github.com>
2025-01-31 12:47:47 +0530

    Add files via upload

---
+++ Coffee Order System/CoffeeDecorator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coffee_Order_System;
+
+/**
+ *
+ * @author kumar
+ */
+public abstract class CoffeeDecorator extends Coffee {
+    public abstract String getDescription();
+}
+
---
+++ Coffee Order System/SugarDecorator.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coffee_Order_System;
+
+/**
+ *
+ * @author kumar
+ */
+public class SugarDecorator extends CoffeeDecorator {
+
+    private Coffee coffee;
+
+    public SugarDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost() + 0.50; // Price for sugar
+    }
+}
